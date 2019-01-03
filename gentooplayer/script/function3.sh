fec2c9d2619973e4df518934feb6b18c9ffbd117
antonellocaroli <antonellocaroli@gmail.com>
2019-01-03 16:18:41 +0100

    up

--- gentooplayer/script/function3.sh
+++ gentooplayer/script/function3.sh
@@ -13,10 +13,10 @@ ramdisk(){
   echo -e " [1]  Ramdisk Reset - to use when you need to change the ramdisk mode or changing the kernel"
   echo -e " [2]  Cleanup Buffer/Cache - to give when you see the ram too full, check with free -m"
   echo -e " \e[38;5;154m----------\e[0m"
-  echo -e " [3]  Ramdisk mode 1 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod"
-  echo -e " [4]  Ramdisk mode 2 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod, RoonBridge"
-  echo -e " [5]  Ramdisk mode 3 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod (B)"
-  echo -e " [6]  Ramdisk mode 4 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod, RoonBridge (B)"
+  echo -e " [3]  Ramdisk mode 1 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod, Upmpdcli"
+  echo -e " [4]  Ramdisk mode 2 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod, RoonBridge, Upmpdcli"
+  echo -e " [5]  Ramdisk mode 3 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod, Upmpdcli (B)"
+  echo -e " [6]  Ramdisk mode 4 - Squeezelite, Squeezelite-R2, Mpd, Networkaudiod, RoonBridge, Upmpdcli (B)"
   echo -e " \e[38;5;154m----------\e[0m"
   echo -e " [7]  RamSave - it works on the fly, the ramdisk automatically restarts after saving"
   echo -e " [8]  Add the ramdisk at system startup - it also works with the system in RAM"
@@ -45,38 +45,38 @@ sync && echo 3 > /proc/sys/vm/drop_caches ;
 echo
 echo -e "$Yellow press a key to continue $Color_Off" ; read -n1 ; ramdisk ;;
 3)
-cp /gentooplayer/script/ramdisk1.sh /gentooplayer/script/ramdisk.sh ;
+cp /gentooplayer/script/ramdisk1.sh /etc/default/ramdisk.sh ;
 echo -e "$Yellow do you want to start the ramdisk? y/n $Color_Off"
 read want
 	if [ "$want" = "y" ]; then
-    /gentooplayer/script/ramdisk.sh
+    /etc/default/ramdisk.sh
     fi ;
 echo
 echo -e "$Yellow press a key to continue $Color_Off" ; read -n1 ; ramdisk ;;
 4)
-cp /gentooplayer/script/ramdisk2.sh /gentooplayer/script/ramdisk.sh ;
+cp /gentooplayer/script/ramdisk2.sh /etc/default/ramdisk.sh ;
 echo -e "$Yellow do you want to start the ramdisk? y/n $Color_Off"
 read want
 	if [ "$want" = "y" ]; then
-    /gentooplayer/script/ramdisk.sh
+    /etc/default/ramdisk.sh
     fi ;
 echo
 echo -e "$Yellow press a key to continue $Color_Off" ; read -n1 ; ramdisk ;;
 5)
-cp /gentooplayer/script/ramdisk3.sh /gentooplayer/script/ramdisk.sh ;
+cp /gentooplayer/script/ramdisk3.sh /etc/default/ramdisk.sh ;
 echo -e "$Yellow do you want to start the ramdisk? y/n $Color_Off"
 read want
 	if [ "$want" = "y" ]; then
-    /gentooplayer/script/ramdisk.sh
+    /etc/default/ramdisk.sh
     fi ;
 echo
 echo -e "$Yellow press a key to continue $Color_Off" ; read -n1 ; ramdisk ;;
 6)
-cp /gentooplayer/script/ramdisk4.sh /gentooplayer/script/ramdisk.sh ;
+cp /gentooplayer/script/ramdisk4.sh /etc/default/ramdisk.sh ;
 echo -e "$Yellow do you want to start the ramdisk? y/n $Color_Off"
 read want
 	if [ "$want" = "y" ]; then
-    /gentooplayer/script/ramdisk.sh
+    /etc/default/ramdisk.sh
     fi ;
 echo
 echo -e "$Yellow press a key to continue $Color_Off" ; read -n1 ; ramdisk ;;
